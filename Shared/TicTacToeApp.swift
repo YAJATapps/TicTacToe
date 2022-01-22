9025458fddae2ae7bc663887a18fdd79c6bb888c
Yajat <yajatapps3@gmail.com>
2022-01-22 02:12:25 -0800

    Initial Commit

---
+++ Shared/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  Shared
+//
+//  Created by Yajat Kumar on 2022-01-22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
